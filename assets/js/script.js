318ff0359629e2f9edb59b3e7515486b9922ee05
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-20 11:12:42 -0800

    added clear entire schedule button that clears local storage and reloads page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var clearStorage = document.getElementById("clear-storage");
+
+
 //variables that grab input
 var nineSave = document.getElementById("nine-save-task");
 var tenSave = document.getElementById("ten-save-task");
@@ -34,10 +37,14 @@ var fifteenTaskStored = JSON.parse(localStorage.getItem("15-task"));
 var sixteenTaskStored = JSON.parse(localStorage.getItem("16-task"));
 var seventeenTaskStored = JSON.parse(localStorage.getItem("17-task"));
 
+
+//inputs current date and time into 
+$("#date").text(currentTime.format('MMMM Do YYYY, h:mm:ss a'));
+
 //functions to store tasks
 function renderNineTask() {    
     document.getElementById("nine-save-task").innerHTML = nineTaskStored;
-  }
+}
 function renderTenTask() {
     document.getElementById("ten-save-task").innerHTML = tenTaskStored;
 }
@@ -63,7 +70,7 @@ function renderSeventeenTask() {
     document.getElementById("seventeen-save-task").innerHTML = seventeenTaskStored;
 }
 
-
+//shows saved tasks upon page reload
 renderNineTask();
 renderTenTask();
 renderElevenTask();
@@ -74,53 +81,39 @@ renderFifteenTask();
 renderSixteenTask();
 renderSeventeenTask();
 
-
-
-//inputs current date and time into 
-$("#date").text(currentTime.format('MMMM Do YYYY, h:mm:ss a'));
-
-
-//event listners that save input
+//functions to save tasks
 nine.addEventListener("click", function() {
     localStorage.setItem("9-task", JSON.stringify(nineSave.value));
 })
-
-
 ten.addEventListener("click", function() {
     localStorage.setItem("10-task", JSON.stringify(tenSave.value));
 })
-
 eleven.addEventListener("click", function() {
     localStorage.setItem("11-task", JSON.stringify(elevenSave.value));
 })
-
 twelve.addEventListener("click", function() {
     localStorage.setItem("12-task", JSON.stringify(twelveSave.value));
 })
-
 thirteen.addEventListener("click", function() {
     localStorage.setItem("13-task", JSON.stringify(thirteenSave.value));
 })
-
 fourteen.addEventListener("click", function() {
     localStorage.setItem("14-task", JSON.stringify(fourteenSave.value));
 })
-
 fifteen.addEventListener("click", function() {
     localStorage.setItem("15-task", JSON.stringify(fifteenSave.value));
 })
-
 sixteen.addEventListener("click", function() {
     localStorage.setItem("16-task", JSON.stringify(sixteenSave.value));
 })
-
 seventeen.addEventListener("click", function() {
     localStorage.setItem("17-task", JSON.stringify(seventeenSave.value));
 })
 
-
-
-//function to save
+clearStorage.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
 
 
 
