73f86e6d84b2eb68a64862458e33983ea793bddb
Jeffery Patterson <jpatterson933@ucla.edu>
2023-03-21 08:57:44 -0700

    1. Added server directory. Put server.js in directory, split mongodb instance to config file called connection.js. Added package.json and nodemon to server directory as well as watch command.

---
+++ server/config/connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+
+// mongoose.connect(
+//     process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-set-up',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     }
+// );
+
+
+mongoose.connect(
+    connectionStringURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+
+);
+
+module.exports = mongoose.connection;
