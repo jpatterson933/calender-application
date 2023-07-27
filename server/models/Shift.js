15b7545d2bb05f08bc185709ae8dd6fd634e2638
jpatterson-cYRYJA <jpatterson933@ucla.edu>
2023-07-27 16:09:31 -0700

    wrote middleware pre function for models to convert times before they are added to database so I dont' have to write tedious front end javascript logic to handle this and can just plug in dates and times that correspond to dates.

--- server/models/Shift.js
+++ server/models/Shift.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // I can add a hook here to convert start and end time to pacific time
 // save the timezone as convertedX with x being the timezone
@@ -21,6 +21,29 @@ const ShiftSchema = new mongoose.Schema({
     }
 
 });
+
+ShiftSchema.pre("save", function (next) {
+    const timeZoneAdjustments = {
+        mountain: 1,
+    }
+
+    if (this.timezone === "mountain") {
+        let startHour = this.startTime.split(":")[0];
+        let startMinute = this.startTime.split(":")[1]
+        let endHour = this.endTime.split(":")[0];
+        let endMinute = this.endTime.split(":")[1]
+
+        const convertedStartTime = startHour - timeZoneAdjustments.mountain;
+        const convertedEndTime = endHour - timeZoneAdjustments.mountain
+
+        this.startTime = `${convertedStartTime}:${startMinute}`;
+        this.endTime = `${convertedEndTime}:${endMinute}`;
+
+        this.timezone = "convertedMountain";
+
+    }
+    next();
+})
 
 const Shift = mongoose.model("Shift", ShiftSchema);
 
