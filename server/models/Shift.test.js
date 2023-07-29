5136d5e76b7862e60012dcf8dfc3393016f5e85a
jpatterson-cYRYJA <jpatterson933@ucla.edu>
2023-07-29 16:08:09 -0700

    need to build out Shift.test.js

--- server/models/Shift.test.js
+++ server/models/Shift.test.js
@@ -14,15 +14,19 @@ describe("Shift model test", () => {
     it("create and save a shift", async () => {
         const shiftData = {
             date: "2023-07-27",
-            timezone: "eastern",
-            startTime: "17:45",
-            endTime: "22:00"
+            timezone: "eastern", // testing failing here // that is a good thing
+            startTime: "17:45", // testing failing here // that is a good thing
+            endTime: "22:00" // testing failing here // that is a good thing
         }
 
         const validShift = new Shift(shiftData);
         const savedShift = await validShift.save();
 
         expect(savedShift._id).toBeDefined();
+        expect(savedShift.date).toBe(shiftData.date);
+        expect(savedShift.timezone).toBe(shiftData.timezone);
+        expect(savedShift.startTime).toBe(shiftData.startTime);
+        expect(savedShift.endTime).toBe(shiftData.endTime);
 
     })
 })
